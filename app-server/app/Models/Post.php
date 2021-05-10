ae2d99db3d3b36c5ff021adb10a88cdb0a851da4
Matsui <k.12ap.m@gmail.com>
2021-05-10 19:45:55 +0900

    Post モデル作成

---
+++ app-server/app/Models/Post.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'user_id', 
+        'city_id',
+        'category_id',
+        'status_id',
+        'titile',
+        'address',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    public function postImage(){
+        return $this->hasOne(postImage::class);
+    }
+}
