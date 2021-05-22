bf1f18bfaaff4c528074694d35344cd5c12cffa5
Matsui <k.12ap.m@gmail.com>
2021-05-22 15:44:01 +0900

    ログイン時バリデーション変更

--- app-server/resources/lang/en/auth.php
+++ app-server/resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'アドレスとパスワードが一致しません。',
+    'throttle' => 'ログイン試行回数が多すぎます。数秒後に再試行してください。',
 
 ];
