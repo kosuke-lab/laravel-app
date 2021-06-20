536aa98e5ea51f46f5379d1e1f3a2faacfaec13f
Matsui <k.12ap.m@gmail.com>
2021-06-20 14:21:26 +0900

    S3追加

--- app-server/resources/views/index.blade.php
+++ app-server/resources/views/index.blade.php
@@ -10,128 +10,126 @@
 @section('content')
   
 
-  <div class="container-fluid">
-    <div class="row">
-  <img src="{{ asset('images/top_mv.jpg')}}" class="img-fluid">
-  </div>
-  </div>
-
-  <div class="block">
-<div class="container">
-  <h2>サービス紹介</h2>
+<div class="container-fluid">
   <div class="row">
-    <div class="col-md-6 col-sm-12">
-      <p>あああああああああああああああああ</p>
+    <img src="{{ asset('images/top_mv.jpg')}}" class="img-fluid">
+  </div>
+</div>
+
+<div class="block">
+  <div class="container">
+    <h2>サービス紹介</h2>
+    <div class="row">
+        <div class="col-md-6 col-sm-12">
+        <p>東京23区内のオススメの観光地や、グルメなど、各場所、ジャンルに合わせてランダムで表示してくれるユーザー投稿型ランダムアプリケーション。東京に引っ越して間もない方や、東京で長く住んでいるが、まだまだ東京のことを知りたい方にとってオススメアプリケーションです。</p>
+        </div>
+          <div class="col-md-6">
+              <div class="circle-image">
+              </div>
+          </div>
     </div>
-     <div class="col-md-6">
-      <div class="circle-image">
-      </div>
-      </div>
   </div>
-  </div>
-  </div>
+</div>
 
-  <div class="block">
-<div class="container" id="search">
-        <h2 class="headingstyle02">23区から探す</h2>
+<div class="block">
+    <div class="container" id="search">
+            <h2 class="headingstyle02">23区から探す</h2>
 
-        <div id="app">
-<form action="{{ route('post.list') }}" method="POST" id="back" autocomplete="off">   
-    {{ csrf_field() }}
-    <div class="">
+      <div id="app">
+      <form action="{{ route('post.list') }}" method="POST" id="back" autocomplete="off">   
+          {{ csrf_field() }}
+
+      <div class="">
+          <div class="clearfix">
+              <div class="border-bottom">
+              <h3>都心</h3>
+              </div>
+                  <div class="float-left">
+                      <div class="radio-tile-group">
+                      @foreach ($cities_center as $city)
+                          <div class="input-container">
+                          <input value="{{$city->id}}" name="cityId" type="radio"  @click="openModal" class="radio-button" autocomplete="on">
+                              <div class="radio-tile">
+                                  <div class="icon">
+                                      <img src="{{ asset('images/' .$city->file_path)}}">
+                                  </div>
+                              <label for="bike" class="radio-tile-label">{{ $city->name }}</label>
+                              </div>
+                          </div>    
+                      @endforeach
+                      </div>
+                  </div>
+      </div>
+
       <div class="clearfix">
-      <div class="border-bottom">
-      <h3>都心</h3>
-        </div>
-        <div class="float-left">
-    <div class="radio-tile-group">
-    @foreach ($cities_center as $city)
-    <div class="input-container">
-    <input value="{{$city->id}}" name="cityId" type="radio"  @click="openModal" class="radio-button" autocomplete="on">
-      <div class="radio-tile">
-      <div class="icon">
-      <img src="{{ asset('images/' .$city->file_path)}}">
-        </div>
-        <label for="bike" class="radio-tile-label">{{ $city->name }}</label>
+              <div class="border-bottom">
+              <h3>副都心</h3>
+              </div>
+                  <div class="float-left">
+                      <div class="radio-tile-group">
+                      @foreach ($cities_subcenter as $city)
+                          <div class="input-container">
+                              <input value="{{$city->id}}" name="cityId" type="radio"  @click="openModal" class="radio-button">
+                                <div class="radio-tile">
+                                  <div class="icon">
+                                      <img src="{{ asset('images/' .$city->file_path)}}" alt="{{$city->file_path}}">
+                                  </div>
+                                <label for="bike" class="radio-tile-label">{{ $city->name }}</label>
+                                </div>
+                          </div>    
+                      @endforeach
+                      </div>
+                  </div>
       </div>
-    </div>    
-   @endforeach
-   </div>
-   </div>
-   </div>
 
-   <div class="clearfix">
-   <div class="border-bottom">
-   <h3>副都心</h3>
-     </div>
-     <div class="float-left">
-   <div class="radio-tile-group">
-   @foreach ($cities_subcenter as $city)
-    <div class="input-container">
-    <input value="{{$city->id}}" name="cityId" type="radio"  @click="openModal" class="radio-button">
-      <div class="radio-tile">
-      <div class="icon">
-      <img src="{{ asset('images/' .$city->file_path)}}" alt="{{$city->file_path}}">
-        </div>
-        <label for="bike" class="radio-tile-label">{{ $city->name }}</label>
+      <div class="clearfix">
+              <div class="border-bottom">
+                <h3>東部</h3>
+              </div>
+                  <div class="float-left">
+                      <div class="radio-tile-group">
+                      @foreach ($cities_east as $city)
+                          <div class="input-container">
+                          <input value="{{$city->id}}" name="cityId" type="radio"  @click="openModal" class="radio-button">
+                              <div class="radio-tile">
+                                  <div class="icon">
+                                      <img src="{{ asset('images/' .$city->file_path)}}">
+                                  </div>
+                              <label for="bike" class="radio-tile-label">{{ $city->name }}</label>
+                              </div>
+                          </div>    
+                      @endforeach
+                      </div>
+                  </div>
       </div>
-    </div>    
-   @endforeach
-   </div>
-   </div>
-   </div>
 
-   <div class="clearfix">
-   <div class="border-bottom">
-   <h3>東部</h3>
-     </div>
-     <div class="float-left">
-   <div class="radio-tile-group">
-   @foreach ($cities_east as $city)
-    <div class="input-container">
-    <input value="{{$city->id}}" name="cityId" type="radio"  @click="openModal" class="radio-button">
-      <div class="radio-tile">
-      <div class="icon">
-      <img src="{{ asset('images/' .$city->file_path)}}">
+      <div class="clearfix">
+              <div class="border-bottom">
+                <h3>西部</h3>
+              </div>
+                  <div class="float-left">
+                      <div class="radio-tile-group">
+                      @foreach ($cities_west as $city)
+                          <div class="input-container">
+                          <input value="{{$city->id}}" name="cityId" type="radio"  @click="openModal" class="radio-button">
+                              <div class="radio-tile">
+                                  <div class="icon">
+                                    <img src="{{ asset('images/' .$city->file_path)}}">
+                                  </div>
+                              <label for="bike" class="radio-tile-label">{{ $city->name }}</label>
+                              </div>
+                          </div>    
+                      @endforeach
+                      </div>
+                  </div>
+      </div> 
+        <open-modal v-show="showContent" @close="showContent = false"  :category_datas="{{ json_encode($categories) }}"></open-modal>
         </div>
-        <label for="bike" class="radio-tile-label">{{ $city->name }}</label>
       </div>
-    </div>    
-   @endforeach
-   </div>
-   </div>
-   </div>
-
-   <div class="clearfix">
-   <div class="border-bottom">
-   <h3>西部</h3>
-   </div>
-     <div class="float-left">
-   <div class="radio-tile-group">
-   @foreach ($cities_west as $city)
-    <div class="input-container">
-    <input value="{{$city->id}}" name="cityId" type="radio"  @click="openModal" class="radio-button">
-      <div class="radio-tile">
-      <div class="icon">
-      <img src="{{ asset('images/' .$city->file_path)}}">
-        </div>
-        <label for="bike" class="radio-tile-label">{{ $city->name }}</label>
-      </div>
-    </div>    
-   @endforeach
-   </div>
-   </div>
-  </div>
-
-   
-   <open-modal v-show="showContent" @close="showContent = false"  :category_datas="{{ json_encode($categories) }}"></open-modal>
-   </div>
-</div>
-</form>
+    </form>
+    </div>
 </div>
 
-</div>
-</div>
 
 
 
