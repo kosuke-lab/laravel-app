f3c64a20a07a886be2b9513a0ad209f3cb49e078
Matsui <k.12ap.m@gmail.com>
2021-06-23 07:50:55 +0900

    bladeファイル修正

--- app-server/resources/views/admin.blade.php
+++ app-server/resources/views/admin.blade.php
@@ -9,7 +9,7 @@
 
 @section('content')
 
-<div class="container">
+<div class="container main">
 
         <p>{{ $statuses[1]}}</p>
     {{ Form::open(['method' => 'get']) }}
--- app-server/resources/views/admin_edit.blade.php
+++ app-server/resources/views/admin_edit.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
 
-<div class="container">
+<div class="container main">
          <h1>お店編集</h1>
          <ul>
         @foreach ($errors->all() as $error)
@@ -15,16 +15,16 @@
          <!-- <form action="{{ route('post.store')}} "enctype="multipart/form-data" method="POST"> -->
          @csrf
         <div class='form-group'>
-            {{ Form::label('titile', '店名:') }}
+            {{ Form::label('titile', '場所:') }}
             {{ Form::text('titile', null,['class' => 'form-control']) }}
         </div>
         <div class='form-group'>
             {{ Form::label('category_id', 'カテゴリー:') }}
-            {{ Form::select('category_id', $categories,'ordinarily', ['class' => 'form-control']) }}
+            {{ Form::select('category_id', $categories,  null, ['class' => 'form-control']) }}
         </div>
         <div class='form-group'>
             {{ Form::label('city_id', '市町村:') }}
-            {{ Form::select('city_id', $cities,'ordinarily', ['class' => 'form-control']) }}
+            {{ Form::select('city_id', $cities , null , ['class' => 'form-control']) }}
         </div>
 
         <div class='form-group'>
@@ -34,7 +34,7 @@
 
         <div class='form-group'>
             {{ Form::label('status_id', 'ステータス:') }}
-            {{ Form::select('status_id', $statuses, $statuses[$post->status_id], ['class' => 'form-control'])}}
+            {{ Form::select('status_id', $statuses,  $statuses[$post->status_id], ['class' => 'form-control'])}}
         </div>
 
         <div class='form-group'> 
