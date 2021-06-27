4b71d25d8411f732fc0ddb27cb6a1140dc7e51bd
Matsui <k.12ap.m@gmail.com>
2021-06-27 16:45:16 +0900

    default追加

--- app-server/database/migrations/2021_06_27_074312_change_city_name_area_to_nullable_to_city_table.php
+++ app-server/database/migrations/2021_06_27_074312_change_city_name_area_to_nullable_to_city_table.php
@@ -14,9 +14,9 @@ class ChangeCityNameAreaToNullableToCityTable extends Migration
     public function up()
     {
         Schema::table('cities', function (Blueprint $table) {
-            $table->string('name')->nullable(false)->change();
-            $table->string('area')->nullable(false)->change();
-            $table->string('file_path')->nullable(false)->change();
+            $table->string('name')->change()->default('');
+            $table->string('name')->change()->default('');
+            $table->string('file_path')->change()->default('');
 
         });
     }
