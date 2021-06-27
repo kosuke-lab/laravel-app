1b11f5fc460e092a75bd1884657c0bbab6fc26c5
Matsui <k.12ap.m@gmail.com>
2021-06-27 16:53:48 +0900

    nullable()に変更後キャッシュクリア

--- app-server/database/migrations/2021_06_27_074312_change_city_name_area_to_nullable_to_city_table.php
+++ app-server/database/migrations/2021_06_27_074312_change_city_name_area_to_nullable_to_city_table.php
@@ -14,9 +14,9 @@ class ChangeCityNameAreaToNullableToCityTable extends Migration
     public function up()
     {
         Schema::table('cities', function (Blueprint $table) {
-            $table->string('name')->change()->default('');
-            $table->string('name')->change()->default('');
-            $table->string('file_path')->change()->default('');
+            $table->string('name')->change()->nullable();
+            $table->string('name')->change()->nullable();
+            $table->string('file_path')->change()->nullable();
 
         });
     }
