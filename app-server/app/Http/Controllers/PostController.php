7c56759fed0285b9e7c40ce0b161140f7ce01894
Matsui <k.12ap.m@gmail.com>
2021-07-08 22:19:51 +0900

    s3パス修正

--- app-server/app/Http/Controllers/PostController.php
+++ app-server/app/Http/Controllers/PostController.php
@@ -118,7 +118,7 @@ class PostController extends Controller
                  //AWSへ画像アップロード
                 Post_image::create([
                     'file_name' => $file_name,
-                    'file_path'=> 'image/'.$file->store('/', 's3'),
+                    'file_path'=> 'laravel-app-images/'.$file->store('/', 's3'),
                      'post_id' =>  $post_id,
                 ]);
             session()->flash('msg_success', '編集しました。');
